91afc5bac068301b25760807bad9940595199c66
Gabriel <gabe.rosa@dialpad.com>
2021-01-25 08:43:09 -0300

    Add project initial structure

---
+++ SwiftUIMovies/SwiftUIMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMoviesApp.swift
+//  SwiftUIMovies
+//
+//  Created by Gabriel Rosa on 1/22/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
